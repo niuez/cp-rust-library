04e60682443930fddd75afe546585351ae28f75c
kutimoti <kutimoti.tokura@gmail.com>
2019-09-23 15:20:21 +0900

    [add] segment_tree

--- src/data_structures/algebra.rs
+++ src/data_structures/algebra.rs
@@ -2,10 +2,6 @@ pub trait Magma: Sized + Clone {
   fn op(&self, rhs: &Self) -> Self;
 }
 
-pub trait MutMagma: Magma {
-    fn mut_op(&mut self, rhs: &Self) { *self = self.clone().op(rhs); }
-}
-
 pub trait Associative: Magma {}
 
 pub trait Unital: Magma {
@@ -16,10 +12,6 @@ pub trait Monoid: Magma + Associative + Unital {}
 
 pub trait Effect<E: Monoid> where Self: Monoid {
     fn effect(&self, e: &E) -> Self;
-}
-
-pub trait MutEffect<E: Monoid> where Self: Effect<E> {
-    fn mut_effect(&mut self, e: &E) { *self = self.clone().effect(e); }
 }
 
 impl<T: Magma + Associative + Unital> Monoid for T {}
--- src/data_structures/segment_tree/mod.rs
+++ src/data_structures/segment_tree/mod.rs
@@ -1,2 +1,1 @@
-pub mod algebra;
 pub mod segment_tree;
