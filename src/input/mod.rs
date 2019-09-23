012e982675c9d186f1f4ec7994014ef41e31f9d5
kutimoti <kutimoti.tokura@gmail.com>
2019-09-23 11:50:22 +0900

    [add test] input

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,31 +1,41 @@
 #[macro_export]
 macro_rules! input {
-    (source = $s: expr, $($r: tt)*) => {
+    (source = $s:expr, $($r:tt)*) => {
         let mut iter = $s.split_whitespace();
-        input_inner!{ iter, $($r)* }
-    }
+        input_rec!{ iter, $($r)* }
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        }
+        let mut iter = s.split_whitespace();
+        input_rec!{ iter, $($r)* }
+    };
 }
 
 #[macro_export]
 macro_rules! input_rec {
-    ($iter: expr) => {};
-    ($iter: expr, ) => {};
-    ($iter: expr, $var:ident : $t:tt $($r: tt)*) => {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt, $($r:tt)*) => {
         let $var = read_value!($iter, $t);
-        input_rec($iter, $($r)*)
-    }
+        input_rec! { $iter, $($r)* }
+    };
 }
 
 #[macro_export]
-macro_rules! real_value {
+macro_rules! read_value {
 
     // tuple
-    ($iter: expr, ( $($t: tt), * )) => {
+    ($iter:expr, ( $($t:tt), * )) => {
         ( $(read_value!($iter, $t)), * )
     };
     
     // array
-    ($iter: expr, [ $t:tt; $len:expr ]) => {
+    ($iter:expr, [ $t:tt; $len:expr ]) => {
         (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
     };
     
@@ -35,7 +45,20 @@ macro_rules! real_value {
     };
     
     // any other
-    ($iter: expr, $t:ty) => {
+    ($iter:expr, $t:ty) => {
         $iter.next().unwrap().parse::<$t>().expect("Parse error")
     };
+}
+
+#[test]
+fn input_test() {
+    input! {
+        source = "2 4\n10 20 30 40",
+        n: usize,
+        m: usize,
+        a: [usize; m],
+    }
+    assert_eq!(n, 2);
+    assert_eq!(m, 4);
+    assert_eq!(a, [10, 20, 30, 40]);
 }
