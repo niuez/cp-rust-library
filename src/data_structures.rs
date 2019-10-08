f733e81cc9f734ac6008e6d0847b9f9c62ff7197
kutimoti <kutimoti.tokura@gmail.com>
2019-10-09 00:32:41 +0900

    [add] node_traits

--- src/data_structures.rs
+++ src/data_structures.rs
@@ -2,3 +2,5 @@ pub mod segment_tree;
 pub mod containers;
 pub mod heap;
 pub mod tree;
+pub mod node_traits;
+pub mod splay_tree;
---
+++ src/data_structures/node_traits.rs
@@ -0,0 +1,33 @@
+use crate::algebra::*;
+
+pub type Link<N> = Option<Box<N>>;
+
+pub trait Node: Sized {
+    type Value;
+    fn push(&mut self);
+    fn fix(&mut self);
+    fn child(&self, dir: usize) -> &Link<Self>;
+    fn take(&mut self, dir: usize) -> Link<Self>;
+    fn set(&mut self, dir: usize, node: Link<Self>);
+    fn val(&self) -> &Self::Value;
+    fn val_mut(&mut self) -> &mut Self::Value;
+}
+
+pub trait ReversibleNode: Node { fn reverse(&mut self); }
+pub trait SizeNode: Node { fn size(&self) -> usize; }
+pub trait FoldNode where Self: Node, <Self as Node>::Value: Monoid {
+    fn fold(&self) -> <Self as Node>::Value;
+}
+
+pub fn size<N: SizeNode>(link: &Link<N>) -> usize {
+    match *link {
+        Some(ref node) => node.size(),
+        None => 0,
+    }
+}
+pub fn fold<N: FoldNode>(link: &Link<N>) -> <N as Node>::Value where <N as Node>::Value: Monoid {
+    match *link {
+        Some(ref node) => node.fold(),
+        None => <N as Node>::Value::identity(),
+    }
+}
